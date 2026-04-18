a3232bb92c49e5f9d7075cd21fe144ff4c7a3b09
Dilobar Xudayarova <xdilobar2197@gmail.com>
2026-04-18 17:52:50 +0500

    Add programming quiz questions to main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+# test2
+
+Konstruktor qaysi method?
+A) start
+B) init
+C) create
+D) new
+self nimani bildiradi?
+A) Klass
+B) Obyekt
+C) Funksiya
+D) Modul
+Klass yaratish kalit so‘zi?
+A) def
+B) class
+C) create
+D) func
+Private atribut belgisi?
+A) _
+B) __
+C) #
+D) &
+Getter nima?
+A) O‘qish
+B) Yozish
+C) O‘chirish
+D) Nusxa olish
+Setter nima?
+A) O‘qish
+B) Yozish
+C) Chop etish
+D) Yashirish
+Static method qanday belgilanadi?
+A) @static
+B) @staticmethod
+C) @method
+D) @func
+Class method belgisi?
+A) @class
+B) @classmethod
+C) @cls
+D) @staticmethod
+super() nima uchun ishlatiladi?
+A) Parent chaqirish
+B) Print
+C) Input
+D) Delete
+str nima qiladi?
+A) String qaytaradi
+B) O‘chiradi
+C) Qo‘shadi
+D) Bo‘ladi
